c96a4dd729fb2e62907258611acc983e152808ff
knielinc <knielinc@student.ethz.ch>
2018-03-08 14:14:58 +0100

    changed some attributes, made it impossible for the goalpositions to get complex

--- StrechTest.py
+++ StrechTest.py
@@ -11,15 +11,17 @@ import scipy.linalg
 WINDOW_HEIGHT = 800
 WINDOW_WIDTH = 800
 
+gravity = 9.81
 
 pygame.init()
 pygame.font.init()
 
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.DOUBLEBUF, 32)
 
 pygame.display.set_caption("Strech Test")
 
 FPS = 60
+clock = pygame.time.Clock()
 currentTime = 0
 lastFrameTime = 0
 
@@ -76,17 +78,17 @@ def draw_loop(dt):
                            0)
         # print("x = " + str(math.floor((curr_point.x_val * WINDOW_WIDTH/2) + WINDOW_WIDTH / 2)) + " y = " + str(math.floor(WINDOW_HEIGHT - ((curr_point.y_val * WINDOW_HEIGHT/2) + WINDOW_HEIGHT / 2))))
         # pygame.draw.circle(window, (255, 255, 255), (100, 100), 50, 0)
-        #label = my_font.render(str(1 / dt), False, (0, 0, 0))
-        #window.blit(label, (0, 0))
-        pygame.display.set_caption("Stretch Test | FPS: " + str(math.floor(1/dt)))
+        # label = my_font.render(str(1 / dt), False, (0, 0, 0))
+        # window.blit(label, (0, 0))
 
 
 def semi_implicit_euler_step(dt):
     for v in current_cluster_configuration:
         v.x_vel = v.x_vel  # ext force in x
-        v.y_vel = v.y_vel - (dt * (9.81 / v.mass))  # gravity
+        v.y_vel = v.y_vel - (dt * (gravity / v.mass))  # gravity
         v.x_pos = v.x_pos + dt * v.x_vel
         v.y_pos = v.y_pos + dt * v.y_vel
+
 
 
 def get_pos_vec(list_p):
@@ -136,14 +138,25 @@ def compute_goal_pos(x0, x1):
     # Aqq symmetric -> no rotational part -> find rotational part of Apq
     # polar decomposition: Apq = R*S -> R = Apq* S^-1
     # R = Apq * sqrt(Apq^t*Apq)^-1
-    #if numpy.linalg.matrix_rank(mat_apq.getH() * mat_apq != (mat_apq.getH() * mat_apq).shape):
+    # if numpy.linalg.matrix_rank(mat_apq.getH() * mat_apq != (mat_apq.getH() * mat_apq).shape):
     #    print(p)
     #    print(mat_apq)
     #    print(mat_apq.T * mat_apq)
-    mat_r = mat_apq * scipy.linalg.inv(scipy.linalg.sqrtm(mat_apq.getH() * mat_apq))
+    mat_test = mat_apq.getH() * mat_apq
+    U, s, V = numpy.linalg.svd(mat_apq.getH() * mat_apq, full_matrices=True)
+    s[s < 0] = 0
+    S = numpy.diag(s)
+    # mat_test_2 = numpy.dot(U, numpy.dot(S, V))
+    mat_r = mat_apq * scipy.linalg.inv(numpy.dot(U, numpy.dot(numpy.diag(numpy.sqrt(s)), V)))
+
+    # mat_r = mat_apq * scipy.linalg.inv(scipy.linalg.sqrtm(mat_apq.getH() * mat_apq))
     # gi = R*qi + xcm
     g = []
     for i in range(0, len(q)):
+        current_g = (mat_r * q[i] + xcm1)
+        current_g_conj = current_g.conj()
+        if not numpy.array_equal(current_g, current_g_conj):
+            print("CARE")
         g.append(mat_r * q[i] + xcm1)
 
     return g
@@ -161,6 +174,7 @@ def project_shape_constraints(k, positions):
         constraint_gradient.append(goal_pos_vec[iter] - curr_pos_vec[iter])
 
     i = 0
+    #print("positions before shape constraint: " + str(get_pos_vec(positions)))
     for curr_pos in positions:
         # print(str(curr_pos.to_vec()) + "before shape" + str(i))
         # print(curr_pos.x_pos)
@@ -171,6 +185,8 @@ def project_shape_constraints(k, positions):
         curr_pos.y_pos = curr_pos.y_pos + (k * constraint_gradient[i].item(1))
         # print(str(curr_pos.to_vec()) + "after shape" + str(i))
         i = i + 1
+    #print("positions after shape constraint: " + str(get_pos_vec(positions)))
+
 
 
 def project_collision_constraints(k, positions):
@@ -188,22 +204,33 @@ def project_collision_constraints(k, positions):
 
     for i in range(0, len(positions)):
         # print(str(positions[i].to_vec()) + "before" + str(i))
-        positions[i].y_pos = positions[i].y_pos + (1 * constraint_gradient[i].item(1)) #assume k = 1
+        positions[i].y_pos = positions[i].y_pos + (.8 * constraint_gradient[i].item(1))  # assume k = 1, but make it a bit less, so that it won't get stuck in an irreversible position
         # print(str(positions[i].to_vec()) + "after" + str(i))
         # print(str(positions[i].y_vel) + " vel y" + str(i))
 
 
-def project_constraints(k, positions):
+def project_velocity_constraints(k, positions, dt):
+    for v in positions:
+        v.x_vel = v.x_vel  # ext force in x
+        v.y_vel = k * (v.y_vel - (dt * (9.81 / v.mass)))  # gravity
+        v.x_pos = v.x_pos + dt * k * v.x_vel
+        v.y_pos = v.y_pos + dt * k * v.y_vel
+
+
+def project_constraints(k, positions, dt):
     project_shape_constraints(k, positions)
 
     project_collision_constraints(k, positions)
+    # project_velocity_constraints(k, positions, dt)
 
 
 solverIterations = 10
 # in [0,1]
-stiffness = .01
+stiffness = .001
 # correct stiffness, so that it is linear to k (stiffness)
 corrected_stiffness = 1 - ((1 - stiffness) ** solverIterations)
+
+
 # print(str(corrected_stiffness) + "lul")
 
 
@@ -212,27 +239,33 @@ def simulation_step(dt):
     semi_implicit_euler_step(dt)
     # generate collision constraints
     for i in range(0, solverIterations):
-        project_constraints(corrected_stiffness, current_cluster_configuration)
+        project_constraints(corrected_stiffness, current_cluster_configuration, dt)
     for curr_point in current_cluster_configuration:
         curr_point.x_vel = (curr_point.x_pos - curr_point.last_x_pos) / dt
         curr_point.last_x_pos = curr_point.x_pos
-        curr_point.y_vel = min((curr_point.y_pos - curr_point.last_y_pos) / dt, 0) # hack
+        curr_point.y_vel = (curr_point.y_pos - curr_point.last_y_pos) / dt # hack
         curr_point.last_y_pos = curr_point.y_pos
     # print(get_pos_vec(current_cluster_configuration))
 
 
-currentTime = time.time()
-lastFrameTime = time.time()
+frame_count = 0
+frame_rate = 0
+frame_rate_t0 = time.process_time()
+frame_rate_t1 = time.process_time()
+currentTime = time.process_time()
+lastFrameTime = time.process_time()
 running = True
 init_scene()
 
 while running:
 
-    sleepTime = 1. / FPS - (currentTime - lastFrameTime)
-    if sleepTime > 0:
-        time.sleep(sleepTime)
-    currentTime = time.time()
-    dt = (currentTime - lastFrameTime)
+    # sleepTime = 1. / FPS - (currentTime - lastFrameTime)
+    # if sleepTime > 0:
+    #    time.sleep(sleepTime)
+    clock.tick(FPS)
+
+    currentTime = time.process_time()
+    dt = 1/FPS#(currentTime - lastFrameTime)
     lastFrameTime = currentTime
 
     for event in pygame.event.get():
@@ -245,6 +278,15 @@ while running:
     window.fill((0, 0, 0))
 
     draw_loop(dt)
+
+    frame_count = frame_count + 1
+
+    if frame_count % 30 == 0:
+        frame_rate_t1 = time.process_time()
+        frame_rate = 30 / (frame_rate_t1 - frame_rate_t0)
+        pygame.display.set_caption("Stretch Test | FPS: " + str(frame_rate))
+        frame_rate_t0 = frame_rate_t1
+
     # pygame.draw.circle(window, (255, 255, 255), (xpos, 100), 50, 0)
 
     pygame.display.flip()
